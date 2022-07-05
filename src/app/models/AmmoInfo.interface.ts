a2019dc10be7b003faf1468f068b130442571c60
Josh Melton <sewdohe@protonmail.com>
2022-07-05 15:52:03 -0400

    woops

---
+++ src/app/models/AmmoInfo.interface.ts
@@ -0,0 +1,10 @@
+// TODO: #1 Make your ammo info model here
+interface AmmoInfo {
+  // ex.
+  // roundsPerBox: number,
+  // costPerBox: number,
+  // ...
+  roundsPerBox: number,
+}
+
+export default AmmoInfo
