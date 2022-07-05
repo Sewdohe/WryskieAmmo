221bc3cd1d26c4a899b449530bd1da931530f8e4
Josh Melton <sewdohe@protonmail.com>
2022-07-05 15:37:10 -0400

    Set up template for MK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import AmmoInfo from "./models/AmmoInfo.interface";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ammoCalc';
+  result?: number = undefined;
+
+  model: AmmoInfo = {
+    // TODO: #2 make blank instance for template model
+    // use crtl + space to auto-complete
+    // the fields you need to make on the object based on your model
+    roundsPerBox: 0, // instantiate everything with the blank value to be shown in the input fields
+  }
+
+  submit() {
+    // TODO: #3 write the logic to calculate the CPR based off of an "AmmoInfo" object.
+    // access properties using this.model.[whateverYouNeed]
+    this.result = 0;
+  }
 }
